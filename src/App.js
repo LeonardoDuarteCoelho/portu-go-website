fb2e45ab38c46f445be2e82502f7ad558116898a
LeonardoDuarteCoelho <leonardo04duarte@gmail.com>
2023-12-10 22:40:44 +0000

    - Section 1 finished - Added new variables for font size and line height

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './components/Header/Header.js';
+import Section1 from './components/Section1/Section1.js';
 
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <Section1 />
     </div>
   );
 }
---
+++ src/components/Section1/Section1.js
@@ -0,0 +1,27 @@
+import './Section1.scss';
+import React from 'react';
+
+const Section1 = () => {
+
+    return(
+        <section className="section-1">
+            <h1 className='section-1__title'>
+                Rápido, fácil e justo
+            </h1>
+            <p className='section-1__text'>
+                <span>PortuGO</span> é uma aplicação de transporte urbano adaptada para suas necessidades. 
+                Basta pedir um veículo em nossa app, que enviaremos um motorista bem-avaliado para atender ao seu chamado. 
+            </p>
+            <div className='section-1__btn-container'>
+                <a href="https://www.apple.com/app-store/">
+                    <img src="./images/vecteezy_app-store-download-button-in-white-colors-download-on-the_12871374_558.png" alt="Botão para a App Store"/>
+                </a>
+                <a href="https://play.google.com/store/games?hl=en&gl=US&pli=1">
+                    <img src="./images/vecteezy_google-play-store-download-button-in-white-colors-download_12871364_282.png" alt="Botão para a Play Store"/>
+                </a>
+            </div>
+        </section>  
+    )
+}
+
+export default Section1;
